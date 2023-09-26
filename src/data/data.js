2caaaa58ee45f39897ef0a69013ea35ff811710a
HANYUJIN <mooo7570@gmail.com>
2023-09-26 22:19:52 +0900

    data test

---
+++ src/data/data.js
@@ -0,0 +1,49 @@
+const hotExhibitions = [
+  {
+    id: '1',
+    posterUrl: 'assets/poster.png',
+    title: '전시이름1',
+    location: '서울',
+    gallery: '갤러리 이름',
+    address: '서울 강남구 학동로 30길 34/ 1층 1호',
+    startDate: '2023-07-04',
+    endDate: '2023-07-10',
+    hours: '11:00 ~ 19:00',
+    entranceFee: '무료',
+    homePage: 'https://www.naver.com/',
+    slug: 'exhibition-detail-1',
+  },
+];
+
+const novExhibitions = [
+  {
+    id: '1',
+    posterUrl: 'assets/poster.png',
+    title: '전시이름1',
+    location: '서울',
+    gallery: '갤러리 이름',
+    address: '서울 강남구 학동로 30길 34/ 1층 1호',
+    startDate: '2023-07-04',
+    endDate: '2023-07-10',
+    hours: '11:00 ~ 19:00',
+    entranceFee: '무료',
+    homePage: 'https://www.naver.com/',
+    slug: 'exhibition-detail-1',
+  },
+];
+
+const hotArtists = [
+  {
+    id: '1',
+    posterUrl:
+      'https://image.harpersbazaar.co.kr/wp-content/uploads/2016/10/bzr_mag_1610_art_19_001-640x847.jpg',
+    artistName: '쥘 드 발랭쿠르',
+    artistEn: 'Jules de Balincourt',
+    field: '회화, 조각',
+    country: '영국',
+    like: 'false',
+    slug: 'artist-detail-1',
+  },
+];
+
+export { hotExhibitions, novExhibitions, hotArtists };
